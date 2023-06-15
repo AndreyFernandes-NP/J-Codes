677d982a3fb57de8431741823ee706a612c93d63
AndreyFernandes-NP <andreyfernandes1361@gmail.com>
2023-06-15 16:32:53 -0300

    QuimioBank

--- J-Database/QuimioBank/src/quimiobank/LC_To_DB.java
+++ J-Database/QuimioBank/src/quimiobank/LC_To_DB.java
@@ -14,8 +14,8 @@ public class LC_To_DB { //"Load & Connect to Database", vai executar os dois num
     
     public static boolean LC(String driverJDBC, String driver) throws InterruptedException {     // A url da conexão assim como o Driver são enviados
         try{                                                                                     // por fora da função, para evitar repetição.
-            System.out.println("//=============================\\\\");                                // O motivo de ter o InterruptedException é por conta
-            System.out.println("//          QuimioBank         \\\\");                                // da função "thread.sleep" que pausa a thread.
+            System.out.println("//=============================\\\\");                         // O motivo de ter o InterruptedException é por conta
+            System.out.println("//          QuimioBank         \\\\");                         // da função "thread.sleep" que pausa a thread.
             System.out.println("//       Para Laboratorios     \\\\"); 
             System.out.println("//=============================\\\\\n");
             System.out.println("QB: Carregando Driver JDBC...");
@@ -25,13 +25,13 @@ public class LC_To_DB { //"Load & Connect to Database", vai executar os dois num
             System.out.printf("QB-ERROR: %s", e);
             return false;
         }
-        try (Connection conn = DriverManager.getConnection(driver, "postgres", "andrey1361")){ //Realiza a conexão com o login.
+        try (Connection conn = DriverManager.getConnection(driver, "postgres", "andrey1361")){ //Realiza a conexão com o login determinado.
             if (conn != null){
                 System.out.println("QB: Conectado ao Banco de Dados, carregando aplicativo...");
                 Thread.sleep(new Random().nextInt(5) * 1000);   //Apenas uma pausa "meme" pro programa.
                 return true;
             }
-        }catch (SQLException e){    //Reportar se houve algum erro SQL na conexão.
+        }catch (SQLException e){    //Reportar se houve algum erro SQL durante a conexão.
             System.out.printf("QB-ERROR, SQL STATE: %s\n%s\n", e.getSQLState(), e.getMessage());
         }
         return false;
--- J-Database/QuimioBank/src/quimiobank/QuimioBank.java
+++ J-Database/QuimioBank/src/quimiobank/QuimioBank.java
@@ -9,15 +9,52 @@ import quimiobank.*; //Importando classes externas que serão usadas...
  * Idealization: Pablo S.; Andrey F.
  */
 
+import java.util.Scanner;
+import java.util.Random; //Mesma little troll de outras classes.
+
 public class QuimioBank {
+    public static Scanner ler = new Scanner(System.in); //Scanner para os Switches.
+    
     public static String driver = "jdbc:postgresql://127.0.0.1:5432/QuimioBank"; //Conexão ao Database.
     public static String driverJDBC = "org.postgresql.Driver"; //Driver à ser utilizado.
 
     public static void main(String[] args) throws InterruptedException{ //Como haverá diversos "Thread.sleep", é necessário declarar um Throw.
-        LC_To_DB startup = new LC_To_DB();
-        if(startup.LC(driverJDBC, driver)){}else{
-            System.out.println("QB-EXCEPTION: FALHA, SUSPENDENDO PROGRAMA...");
+        LC_To_DB startup = new LC_To_DB();  //Aqui será criada a classe que realizará a conexão.
+        
+        if(startup.LC(driverJDBC, driver)){}else{                                   //Se o valor retornado for 'True', prossiga, caso contrário, 
+            System.out.println("QB-EXCEPTION: FALHA, SUSPENDENDO PROGRAMA...");   //o programa se encerra com uma mensagem de erro.
+            return;
         }
+        loop:
+        while(true){
+            System.out.print("\033[H\033[2J");
+            System.out.println("//=============================\\\\");
+            System.out.println("//          QuimioBank         \\\\");
+            System.out.println("//       Seletor de Opcoes     \\\\");
+            System.out.println("//=============================\\\\\n");
+            
+            System.out.println("1 - Criar Tabela no Banco de Dados.");
+            System.out.println("2 - Modificar Tabela ja existente.");
+            System.out.println("Para sair, digite qualquer outro numero.\n");
+            
+            System.out.print("Escolha: ");
+            switch(ler.nextInt()){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break loop;
+            }
+        }
+        System.out.print("\033[H\033[2J");
+        System.out.println("//=============================\\\\");
+        System.out.println("//          QuimioBank         \\\\");
+        System.out.println("//       Para Laboratorios     \\\\"); 
+        System.out.println("//=============================\\\\\n");
+        
+        Thread.sleep(new Random().nextInt(5) * 1000);
+        return;
     }
     
 }
